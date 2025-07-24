08cb8b37d0820602df13786eda03d8b5978196f2
Bruno Pavanelli <bspavanelli@gmail.com>
2025-07-24 14:57:13 -0300

    removido SO não utilizado

--- Assets/Scripts/ScriptableObjects/BuffsSO.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "BuffsSO", menuName = "Scriptable Objects/BuffsSO")]
-public class BuffsSO : ScriptableObject
-{
-    public string buffName;
-    public GameObject prefab;
-}
